acea6476a031adb57ca8915c43d961379cfecac3
Jordan Hart <p22jhart@fairfieldprep.org>
2022-02-02 07:13:31 -0800

    qu

---
+++ quiz.py
@@ -0,0 +1,23 @@
+#prices = [30, 40, 25, 55, 60, 80, 65]
+
+#prices += '-5'
+#lst = prices
+
+#for i, v in enumerate(lst):
+   #  print(i,v)
+
+
+#prices = [30, 40, 25, 55, 60, 80, 65]
+
+#prices += '45'
+
+#lst = [['tee-shirt','long-sleeved','tanktop'],['quarter-zip','pullover','full-zip','half-zip']]
+
+#for i, v in enumerate(lst):
+     #print(i,v)
+
+
+prices= [30, 40, 25, 55, 60, 80, 65]
+
+  return 30+40+25+55+60+80+6
+
